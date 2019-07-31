3a9f48cefab466d33f5006f221ce76019f55d0b1
Jan <6695807+kaestnja@users.noreply.github.com>
2019-07-31 18:12:27 +0200

    Add files via upload

---
+++ add_subfolder_todefender.ps1
@@ -0,0 +1,14 @@
+﻿## Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
+
+$folders = get-childitem D:\ -directory
+echo $folders
+pause
+foreach ($folder in $folders)
+ {
+ $folderexception = $folder.fullname + "\" + ($folder.name)
+ echo $folderexception
+ # Add-MpPreference -ExclusionPath “C:\Temp”
+ Add-MpPreference -ExclusionPath $folderexception
+ pause
+ }
+
