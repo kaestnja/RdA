d7c3f9ef62b3ff1aa061e42dce624a685bc11d80
Jan Kästner <jan.kaestner@live.de>
2019-08-11 16:52:15 +0200

    jk

--- PSRdA/set_trust.ps1
+++ PSRdA/set_trust.ps1
@@ -1,4 +1,10 @@
 Get-Host | Select-Object Version
 Get-PSRepository
+#repair default:
+#Register-PSRepository -Default -InstallationPolicy Trusted
 Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted
 Update-Module
+#Register-PSRepository -Name "myNuGetSource" -SourceLocation "https://www.myget.org/F/powershellgetdemo/api/v2" -PublishLocation "https://www.myget.org/F/powershellgetdemo/api/v2/Packages" -InstallationPolicy Trusted
+#https://github.com/nightroman/Mdbc
+#https://dev.to/tunaxor/powershell--mongo-34om
+#Install-Module Mdbc
