5804f361a123d6b516ad0e209f4a2ff0d1f2130d
Jan Kästner <jan.kaestner@live.de>
2021-07-16 09:31:35 +0200

    jk 6

--- theRadio/anneradio.py
+++ theRadio/anneradio.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-version=184
+version=185
 
 #      nano /home/pi/.config/lxsession/LXDE-pi/autostart # and enter the following lines or:
 # sudo nano /etc/xdg/lxsession/LXDE-pi/autostart # and enter the following lines:
