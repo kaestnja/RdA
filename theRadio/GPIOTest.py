a69e8f9912ccb21cdada99b7f9ac1bfc494d23c5
Jan Kästner <jan.kaestner@online.de>
2021-07-20 22:07:53 +0100

    jk

--- theRadio/GPIOTest.py
+++ theRadio/GPIOTest.py
@@ -6,7 +6,7 @@ CLOCKPIN1 = 23
 DATAPIN1 = 24
 SWITCHPIN1 = 18
 
-CLOCKPIN2 = 27
+CLOCKPIN2 = 4 #27
 DATAPIN2 = 17
 SWITCHPIN2 = 22
 
@@ -24,7 +24,8 @@ def switchPressed2():
 GPIO.setmode(GPIO.BCM)
 
 #ky0401 = KY040(CLOCKPIN1, DATAPIN1, SWITCHPIN1, rotaryChange1, switchPressed1, rotaryBouncetime=50, switchBouncetime=500)
-ky0402 = KY040(CLOCKPIN2, DATAPIN2, rotaryCallback=rotaryChange2, rotaryBouncetime=50)
+#ky0402 = KY040(CLOCKPIN2, DATAPIN2, rotaryCallback=rotaryChange2, rotaryBouncetime=50)
+ky0402 = KY040(CLOCKPIN2, DATAPIN2, SWITCHPIN2, rotaryChange2, switchPressed2, rotaryBouncetime=50, switchBouncetime=500)
 
 #ky0401.start()
 ky0402.start()
